104b21a8f400eba94cedae8cfa9770b6d1d37018
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-04-09 21:41:05 +0900

    [Silver I] Title: 하노이 탑, Time: 896 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,13 @@
+def move(no, x, y):
+    if no > 1:
+        move(no - 1, x, 6 - x - y)
+
+    print(x,y)
+
+    if no > 1:
+        move(no - 1, 6 - x - y, y)
+
+n = int(input())
+print((2**n)-1)
+if n <= 20:  
+    move(n, 1, 3)
