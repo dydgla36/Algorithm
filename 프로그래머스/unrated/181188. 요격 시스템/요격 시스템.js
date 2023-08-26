bca81eea6093d7b7745bed2cc76a5674c5091cba
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-08-26 18:23:23 +0900

    [unrated] Title: 요격 시스템, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,15 @@
+function solution(targets) {
+    let answer = 0;
+    let end = 0;   
+
+    targets.sort((a, b) => a[1] - b[1]);    //오름차순
+    
+    for(let [s, e] of targets) {
+        if(end <= s) {
+            answer++;
+            end = e; 
+        }
+    }
+    
+    return answer;
+}
