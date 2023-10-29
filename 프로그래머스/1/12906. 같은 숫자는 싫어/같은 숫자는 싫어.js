b348f9cc1aa3fe5d9641ca0f4890ac11b74156e2
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-10-29 18:12:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.39 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr)
+{
+    let answer = arr;
+    let result = [];
+    for (let i=0; i<answer.length; i++){
+        if(answer[i]!==answer[i+1]) result.push(arr[i]);
+    }
+    return result;
+}
