f205f64adee9fb588f6828f2a7290a046737beca
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-11-10 04:23:56 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 97 ms, Memory: 45,288 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -0,0 +1,22 @@
+for tc in range(1, 11):
+    n = int(input())
+    or_pw = list(map(int, input().split()))
+    com_num = int(input())
+    com = list(map(str, input().split()))
+
+    for i in range(len(com)):
+        if com[i] == 'I':
+            x = int(com[i+1])
+            y = int(com[i+2])
+            for j in range(y):
+                or_pw.insert(x+j, com[i+3+j])
+
+    print(f'#{tc}', end=" ")
+    print(*or_pw[:10])
+
+
+
+
+
+
+
