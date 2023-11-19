1d79749b85a646dd989aaf57618894abe30a3c92
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-11-19 12:46:52 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 2,926 ms, Memory: 71,212 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,16 +1,16 @@
 for tc in range(1, 11):
     n = int(input())
     data = [list(map(str, input())) for _ in range(100)]
+    r_data = list(map(list, zip(*data)))  # data의 행과 열을 바꿈
     max_length = 0
-    for length in range(100, 0, -1):  
+    for length in range(100, 0, -1):
         for i in range(100):
             for j in range(100 - length + 1):
                 if data[i][j:j+length] == data[i][j:j+length][::-1]:
                     max_length = length
-        for j in range(100):
-            for i in range(100 - length + 1):
-                col = [data[k][j] for k in range(i, i+length)]
-                if col == col[::-1]:
+        for i in range(100):
+            for j in range(100 - length + 1):
+                if r_data[i][j:j+length] == r_data[i][j:j+length][::-1]:  # r_data를 사용하여 각 열에서 회문 찾기
                     max_length = length
         if max_length:
             break
