38ebe08dd30e51de464b077d3fc39c9cfb603970
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-11-19 12:53:20 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 169 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.py
@@ -1,5 +1,11 @@
+def power(n, m):
+    if m == 0:
+        return 1
+    else:
+        return n * power(n, m-1)
+
 for tc in range(1, 11):
-    t = int(input())
-    n, m = map(int,input().split())
-    value = n ** m
-    print(f'#{t} {value}')
+    test_case = int(input())
+    n, m = map(int, input().split())
+    result = power(n, m)
+    print(f'#{test_case} {result}')
