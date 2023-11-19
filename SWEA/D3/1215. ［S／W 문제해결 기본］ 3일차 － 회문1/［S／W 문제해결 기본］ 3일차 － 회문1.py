d6cc1247e7b8045e4d620fd7aa495eec595401f1
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2023-11-19 12:38:00 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 148 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,13 +1,14 @@
 for tc in range(1, 11):
     n = int(input())
     data = [list(map(str, input())) for _ in range(8)]
+    r_data = list(map(list, zip(*data)))  # data의 행과 열을 바꿈
     cnt = 0
     for i in range(8):
         for j in range(8 - n + 1):
             if data[i][j:j+n] == data[i][j:j+n][::-1]:
                 cnt += 1
-    for i in range(8 - n + 1):
-        for j in range(8):
-            if [data[k][j] for k in range(i, i+n)] == [data[k][j] for k in range(i, i+n)][::-1]:
+    for i in range(8):
+        for j in range(8 - n + 1):
+            if r_data[i][j:j+n] == r_data[i][j:j+n][::-1]:  # r_data를 사용하여 각 열에서 회문 찾기
                 cnt += 1
     print(f'#{tc} {cnt}')
