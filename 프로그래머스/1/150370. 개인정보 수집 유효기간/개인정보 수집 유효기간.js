a2bb1f29539b00605f1a7d7b615db5fc92e8550c
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2024-02-02 16:42:26 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,17 @@
+function solution(today, terms, privacies) {
+    let answer = [];
+    let [year, month, date] = today.split(".").map(Number);
+    let todates = year * 12 * 28 + month * 28 + date;
+    let data = {};
+    terms.forEach((e) => {
+        let [a, b] = e.split(" ");
+        data[a] = Number(b);
+    });
+    privacies.forEach((e, i) => {
+        let [day, term] = e.split(" ");
+        day = day.split(".").map(Number);
+        let dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + data[term] * 28;
+        if (dates <= todates) answer.push(i + 1);
+    });
+    return answer;
+}
