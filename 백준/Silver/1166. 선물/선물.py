e020975c013667faf58c68f64979fb24fc7b7716
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2024-02-29 00:14:28 +0900

    [Silver III] Title: 선물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1166. 선물/선물.py
@@ -0,0 +1,11 @@
+N,L,W,H = map(int,input().split())
+start = 0
+end = max(L,W,H)
+for i in range(10000):
+    mid = (start + end) / 2
+    num = (W // mid) * (H // mid) * (L // mid)
+    if num >= N:
+        start = mid
+    else:
+        end = mid
+print("%.10f" %(end))
