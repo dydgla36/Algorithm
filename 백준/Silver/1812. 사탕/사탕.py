31852f6b9b8d694be85c052276a804a45e915937
dydgla36 <128681885+dydgla36@users.noreply.github.com>
2024-03-27 21:14:49 +0900

    [Silver IV] Title: 사탕, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1812. 사탕/사탕.py
@@ -0,0 +1,11 @@
+n = int(input())
+s = 0
+li = []
+for i in range(n):
+    li.append(int(input()))  
+    s += li[i] * (-1)**i
+a = s // 2
+print(a)
+for i in range(n-1):
+    a = li[i] - a
+    print(a)
